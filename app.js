5d927912d3b3e1b601185b89acda4a650e8fbeb8
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-07-04 23:39:05 -0300

    API - Cadastro de Produtos

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { randomUUID } = require("crypto"); // -> gerar id universal
+const { response } = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-const product = [];
+const products = [];
 
 /**
  *  Body -> Sempre que quiser enviar dados para minha aplicação
@@ -12,12 +14,35 @@ const product = [];
  *  Query -> /product/id=12312312312312value=312342134123124
  */
 
-app.post("/products", (request, reponse) => {
+app.post("/products", (request, response) => {
     // Nome e Preço => Name and Price
 
-    const body = request.body;
+    const { name, price } = request.body;
 
-    console.log(body);
+    const product = ({
+        name,
+        price,
+        id: randomUUID(),
+    });
+    
+
+    products.push(product); 
+
+    return response.json(product);
 });
+
+
+app.get("/products", (request, response) => { // => GET, buscar informação.
+    return response.json(products);
+});
+
+app.get("/products/:id", (resquest, response) => {
+    const {id } = resquest.params;
+    const product = products.find(product => product.id === id); // -> vai percorrer o array até encontrar um ID igual
+    return response.json(product);
+});
+
+
+
 
 app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
